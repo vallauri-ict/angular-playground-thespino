c37f85b1c8cf688ea7e817ae76f4ba99d07d66ab
spinoni-giorgio <spinoni-giorgio@users.noreply.github.com>
2020-03-04 14:52:43 +0100

    angular-course first commit
    Project initialization
    Components creation
    Added Bootstrap 3

---
+++ angular-course/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServerFarmComponent } from './server-farm/server-farm.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServerFarmComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
